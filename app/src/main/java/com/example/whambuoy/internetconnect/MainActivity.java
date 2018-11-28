a62255d8278b3eedcd4790c636722adaf5f2313c
Whambuoy <esther25maina@gmail.com>
2018-11-28 12:43:47 +0300

    Menu added

--- app/src/main/java/com/example/whambuoy/internetconnect/MainActivity.java
+++ app/src/main/java/com/example/whambuoy/internetconnect/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.whambuoy.internetconnect;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
 
+    }
 
+    //To create the menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+    //Handle menu item selection
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search){
+            Context context =MainActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, "textToShow", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
